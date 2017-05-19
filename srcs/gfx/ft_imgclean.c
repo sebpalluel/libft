6e782ccff775d908133cfe0a6fb8a54ac1cb5072
sebpalluel <sebpalluel@free.fr>
2017-05-19 15:49:33 +0200

    correct ft_imgclean

--- srcs/gfx/ft_imgclean.c
+++ srcs/gfx/ft_imgclean.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 14:16:07 by psebasti          #+#    #+#             */
-/*   Updated: 2017/04/11 14:16:15 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/05/19 15:49:16 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,5 @@
 
 void				ft_imgclean(t_img *img, size_t width, size_t height)
 {
-	t_pix			pix;
-	unsigned int	index;
-
-	pix.y = 0;
-	while (pix.y < (int)height)
-	{
-		pix.x = 0;
-		while (pix.x < (int)width)
-		{
-			index = (pix.y * img->size_x) + (pix.x * (img->bbp >> 3));
-			img->image_addr[index] = 0;
-			img->image_addr[index + 1] = 0;
-			img->image_addr[index + 2] = 0;
-			pix.x++;
-		}
-		pix.y++;
-	}
+	ft_bzero(img->image_addr, width * height * img->bbp / 8);
 }
