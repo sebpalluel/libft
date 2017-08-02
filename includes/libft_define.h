18f79cb280de60afb6117d67105f48629db95826
sebpalluel <sebpalluel@free.fr>
2017-08-02 17:30:20 +0200

    add numeric key to define

--- includes/libft_define.h
+++ includes/libft_define.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/01 18:47:08 by psebasti          #+#    #+#             */
-/*   Updated: 2017/08/01 18:49:49 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/08/02 17:29:57 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,30 @@
 # define CTRL		256
 # define STAR		67
 # define SLASH		75
-# define G_KEY		5
-# define W_KEY		13
-# define S_KEY		1
 # define A_KEY		0
+# define S_KEY		1
 # define D_KEY		2
+# define F_KEY		3
+# define G_KEY		5
+# define Z_KEY		6
+# define X_KEY		7
+# define C_KEY		8
+# define V_KEY		9
 # define Q_KEY		12
+# define W_KEY		13
 # define E_KEY		14
 # define R_KEY		15
-# define F_KEY		3
 # define L_KEY		37
+# define 0_KEY		29
+# define 1_KEY		18
+# define 2_KEY		19
+# define 3_KEY		20
+# define 4_KEY		21
+# define 5_KEY		23
+# define 6_KEY		22
+# define 7_KEY		26
+# define 8_KEY		28
+# define 9_KEY		25
 
 # define MOUSE_LEFT 1
 # define MOUSE_RIGHT 2
