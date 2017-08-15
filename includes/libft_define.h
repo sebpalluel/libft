b853905c850f986a3a38138afe18d27acfb6478d
sebpalluel <sebpalluel@free.fr>
2017-08-15 11:50:38 +0200

    complete define for minilibx interaction with X11

--- includes/libft_define.h
+++ includes/libft_define.h
@@ -6,63 +6,97 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/01 18:47:08 by psebasti          #+#    #+#             */
-/*   Updated: 2017/08/02 18:03:05 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/08/15 11:50:14 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_DEFINE_H
 # define LIBFT_DEFINE_H
 
-# define MAX_INT 2147483647
-# define MIN_INT -2147483648
-# define BUFF_SIZE 	50
-# define FD_MAX		256
-# define READ_OK	1
-# define READ_EOF	0
-# define READ_ERR	-1
+# define MAX_INT					2147483647
+# define MIN_INT					-2147483648
+# define BUFF_SIZE 					50
+# define FD_MAX						256
+# define READ_OK					1
+# define READ_EOF					0
+# define READ_ERR					-1
 
-# define ESC		53
-# define LEFT		123
-# define RIGHT		124
-# define DOWN		125
-# define UP			126
-# define MINUS		27
-# define EQUAL		24
-# define DOT_KEY	47
-# define SLASH_KEY	44
-# define CTRL		256
-# define STAR		67
-# define SLASH		75
-# define A_KEY		0
-# define S_KEY		1
-# define D_KEY		2
-# define F_KEY		3
-# define G_KEY		5
-# define Z_KEY		6
-# define X_KEY		7
-# define C_KEY		8
-# define V_KEY		9
-# define Q_KEY		12
-# define W_KEY		13
-# define E_KEY		14
-# define R_KEY		15
-# define P_KEY		35
-# define L_KEY		37
-# define ZERO		29
-# define ONE		18
-# define TWO		19
-# define THREE		20
-# define FOUR		21
-# define FIVE		23
-# define SIX		22
-# define SEVEN		26
-# define EIGHT		28
-# define NINE		25
+# define ESC						53
+# define LEFT						123
+# define RIGHT						124
+# define DOWN						125
+# define UP							126
+# define MINUS						27
+# define EQUAL						24
+# define DOT_KEY					47
+# define SLASH_KEY					44
+# define CTRL						256
+# define STAR						67
+# define SLASH						75
+# define A_KEY						0
+# define S_KEY						1
+# define D_KEY						2
+# define F_KEY						3
+# define G_KEY						5
+# define Z_KEY						6
+# define X_KEY						7
+# define C_KEY						8
+# define V_KEY						9
+# define Q_KEY						12
+# define W_KEY						13
+# define E_KEY						14
+# define R_KEY						15
+# define P_KEY						35
+# define L_KEY						37
+# define ZERO						29
+# define ONE						18
+# define TWO						19
+# define THREE						20
+# define FOUR						21
+# define FIVE						23
+# define SIX						22
+# define SEVEN						26
+# define EIGHT						28
+# define NINE						25
 
-# define MOUSE_LEFT 1
-# define MOUSE_RIGHT 2
-# define MOUSE_SIDE 3
-# define WHEEL_UP 4
-# define WHEEL_DOWN 5
+# define MOUSE_LEFT					1
+# define MOUSE_RIGHT				2
+# define MOUSE_SIDE					3
+# define WHEEL_UP					4
+# define WHEEL_DOWN					5
+
+# define MOTION_KEYPRESS			2
+# define MOTION_KEYRELEASE			3
+# define MOTION_BUTTONPRESS			4
+# define MOTION_BUTTONRELEASE		5
+# define MOTION_NOTIFY				6
+# define MOTION_EXPOSE				12
+
+# define NOEVENTMASK				0L
+# define KEYPRESSMASK				(1L<<0)
+# define KEYRELEASEMASK				(1L<<1)
+# define BUTTONPRESSMASK			(1L<<2)
+# define BUTTONRELEASEMASK			(1L<<3)
+# define ENTERWINDOWMASK			(1L<<4)
+# define LEAVEWINDOWMASK			(1L<<5)
+# define POINTERMOTIONMASK			(1L<<6)
+# define POINTERMOTIONHINTMASK		(1L<<7)
+# define BUTTON1MOTIONMASK			(1L<<8)
+# define BUTTON2MOTIONMASK			(1L<<9)
+# define BUTTON3MOTIONMASK			(1L<<10)
+# define BUTTON4MOTIONMASK			(1L<<11)
+# define BUTTON5MOTIONMASK			(1L<<12)
+# define BUTTONMOTIONMASK			(1L<<13)
+# define KEYMAPSTATEMASK			(1L<<14)
+# define EXPOSUREMASK				(1L<<15)
+# define VISIBILITYCHANGEMASK		(1L<<16)
+# define STRUCTURENOTIFYMASK		(1L<<17)
+# define RESIZEREDIRECTMASK			(1L<<18)
+# define SUBSTRUCTURENOTIFYMASK		(1L<<19)
+# define SUBSTRUCTUREREDIRECTMASK	(1L<<20)
+# define FOCUSCHANGEMASK			(1L<<21)
+# define PROPERTYCHANGEMASK			(1L<<22)
+# define COLORMAPCHANGEMASK			(1L<<23)
+# define OWNERGRABBUTTONMASK		(1L<<24)
 
 #endif
