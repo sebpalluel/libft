9b6cd743dcb0e5dfa6e36da6cc1ad2306c9215c0
sebpalluel <sebpalluel@free.fr>
2017-10-22 20:59:21 +0200

    remove abs and replace by ft_abs

--- srcs/int/ft_intlen.c
+++ srcs/int/ft_intlen.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/16 18:53:11 by psebasti          #+#    #+#             */
-/*   Updated: 2017/10/13 14:35:10 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/10/22 20:59:03 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_intlen(int nbr)
 		return (ret);
 	if (nbr < 0)
 	{
-		nbr = abs(nbr);
+		nbr = ft_abs(nbr);
 		ret++;
 	}
 	while (nbr != 0)
