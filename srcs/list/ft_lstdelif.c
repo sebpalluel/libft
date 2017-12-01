7ef49df897c22d0385b95941393327685317884f
psebasti <psebasti@42.fr>
2017-12-01 17:18:51 +0100

    ft_lstdelif now perfectly work in any case

--- srcs/list/ft_lstdelif.c
+++ srcs/list/ft_lstdelif.c
@@ -6,53 +6,54 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 12:03:37 by psebasti          #+#    #+#             */
-/*   Updated: 2017/12/01 14:39:26 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/12/01 16:42:49 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_lstdelif2(t_list **list, void *to_cmpr, \
+static void	ft_lstdelif2(t_list **head, void *to_cmpr, \
 		int (*cmp)(void *, void *), void (*del)(void *, size_t))
 {
 	t_list	*lst_swap;
 	t_list	*lst;
 
 	lst_swap = NULL;
-	lst = *list;
-	while (lst && lst->next && (*cmp)(lst->content, to_cmpr) != OK)
+	lst = *head;
+	while (lst->next && (*cmp)(lst->next->content, to_cmpr) != OK)
 		lst = lst->next;
-	if (lst && lst->next)
+	if (lst)
 	{
-		if (lst->next->next)
+		if (lst->next && lst->next->next)
 		{
 			lst_swap = lst->next->next;
 			ft_lstdelone(&lst->next, del);
 			lst->next = lst_swap;
 		}
-		else
+		else if (lst->next)
 		{
 			ft_lstdelone(&lst->next, del);
 			lst->next = NULL;
 		}
 	}
-	*list = lst;
+	*head = lst;
 }
 
-void		ft_lstdelif(t_list **alst, void *to_cmpr, \
+void		ft_lstdelif(t_list **head, void *to_cmpr, \
 		int (*cmp)(void *, void *), void (*del)(void *, size_t))
 {
 	t_list	*list;
 	t_list	*lst_swap;
 
-	list = *alst;
-	if (*alst && cmp && del)
+	list = *head;
+	if (*head && cmp && del)
 	{
 		if ((*cmp)(list->content, to_cmpr) == OK)
 		{
 			lst_swap = list->next;
 			ft_lstdelone(&list, del);
-			*alst = lst_swap;
+			*head = lst_swap;
+			list = *head;
 		}
 		while (list && list->next)
 			ft_lstdelif2(&list, to_cmpr, cmp, del);
